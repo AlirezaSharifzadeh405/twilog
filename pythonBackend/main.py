11d36d12e4d676e49f5068e3bef0b3d55e91b521
saeidehyavari <52570130+saeidehyavari@users.noreply.github.com>
2019-07-17 20:21:57 +0430

    یک کامیت دیگر
    توضیحی ندارم برای آن

--- pythonBackend/main.py
+++ pythonBackend/main.py
@@ -1,2 +1,26 @@
 print("Hello")
 print("This is Backedn file")
+# ÎÈ ÍÇáÇ ãÊæÌå ÔÏíÏí ØæÑ ˜ÇãíÊ ˜äíÏ¿
+#Èáå
+# ãíÊæäã ÇíãíáÊæä ˜å ÏÑ íÊåÇÈ ËÈÊ äÇã ˜ÑÏíÏ Ñæ ÇíäÌÇ ÈäæíÓíÏ¿
+#saeedeh.yavariab@gmail.com
+#please wait
+# ÎÈ ÇáÇä ÔãÇ Ñæ ÇÖÇİå ˜ÑÏíã Èå ÇÚÖÇí Ñæå
+# ÔãÇ ÈÚÏ ÇÒ Çíä˜å ˜ÇãíÊ ˜ÑÏíÏ æ åÑ æŞÊ ÎæÇÓÊíÏ İÇíá åÇíÊÇä ÈÑ Ñæí ÓÑæÑ ŞÑÇÑ ÈíÑÏ
+# æ ˜ÇãíÊ åÇíÊÇä ĞÎíÑå ÔæÏ (ÈÑ Ñæí ÓÑæÑæ ĞÎíÑå ÔæÏ) ÈÇíÏ Âä ÑÇ push ˜äíÏ
+# ÊÇ ÇíäÌÇ Çæ˜í¿
+#Èáå
+# ÎÈ ÍÇáÇ ãíÊæäíÏ æÇÑÏ ÓÇíÊ github ÈÔíÏ æ È
+#ba username va passwordetan vared shavid?
+# ÓæÇáí ÈæÏ¿¿¿
+# varede site github shavid va ba username va passwordetan vared shavid
+# wait
+# passwordetun ru bezanid tu in site
+#íÇÏã äíÓ
+# ok emailetun k yadetune?
+#Èáå
+# khob didid ?
+# ãä Ïæ ÈÇÑå Çíä İÇíá åÇ ÑÇ ĞÎíÑå ãí ˜äã æ ÈÚÏÔ ˜ÇãíÊ æ ÈÚÏ Ï˜ãå push
+#Ñæ ãíÒäã ÊÇ ÊÛííÑÇÊ ÏÑ ˜Ï ãæÌæÏ ÈÑ Ñæí íÊåÇÈ äíÒ ÇÚãÇá ÔæÏ.
+#Çæ˜í¿
+#Èáå
