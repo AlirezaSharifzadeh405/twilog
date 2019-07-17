af9aa422203effe7b6e58d33a39ce8b2b9212832
saeidehyavari <52570130+saeidehyavari@users.noreply.github.com>
2019-07-17 19:27:08 +0430

    the seconds commit for comment
    this is description...

--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
 print("Hello This Is Sample File")
+# first commit occured
+# i want change this file again and then recommit
+#  ÈÚÏ ÇÒ Çíä˜å åÑ ÈÇÒ İÇíáí Èå Ñæå ÇÖÇİå ˜ÑÏíÏ
+# æ íÇ Çíä˜å İÇíáí ÇÒ Ñæå ÑÇ ÊÛííÑ ÏÇÏíÏ ÈÇíÏ commit ˜äíÏ
+# ÊÇ Èå ÚäæÇä í˜ äŞØå ĞÎíÑå ÔÏå ÇÒ ˜Ï ÔãÇ äåÏÇÑí ˜äÏ
+# åÑ ˜ÇãíÊ í˜ ÇÓã ÏÇÑÏ ˜å ãÇ ãæŞÚ ˜ÇãíÊ Èå Âä ãí Ïåíã
+# ãËáÇ ãä ÈÑÇí Çíä ˜ÇãíÊ ŞÑÇÑ ãí Ïåã:
+# the seconds commit for comment
